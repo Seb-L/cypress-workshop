9cee9120eff80a3070fc40fa9ffe0219bd864e9a
Sébastien Lombard <sebastien@agorize.com>
2018-11-16 16:10:47 +0100

    update tests

--- tests/e2e/specs/result/test-1.js
+++ tests/e2e/specs/result/test-1.js
@@ -28,15 +28,15 @@ describe('Test 1', () => {
       .should('contain', 'Cart')
 
     // Check if one item is in the cart
-    cy.get('[data-cy="cart-item-1"]')
-      .should('exist')
+    cy.get('[data-cy=cart-item]')
+      .should('have.length', 1)
 
     // Check if the first product title is "Product 1"
-    cy.get('[data-cy="item-title-1"]')
+    cy.get('[data-cy=cart-item]')
       .should('contain', 'Product 1')
 
     // Click on first cart item remove button
-    cy.get('[data-cy="remove-btn-1"]')
+    cy.get('[data-cy=cart-item] [data-cy="remove-btn"]')
       .click()
 
     // Check if cart is empty
